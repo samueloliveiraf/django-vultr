addce13fc921e0e90701955e44d98dd0af2a5d4d
samueloliveiraf <samueldiablo73@gmail.com>
2021-12-22 10:07:26 -0300

    add ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -132,9 +132,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/static/')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
