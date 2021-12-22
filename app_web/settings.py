68a9339f0dda82b01be406fe13e058cdcbd9601f
samueloliveiraf <samueldiablo73@gmail.com>
2021-12-22 17:04:39 -0300

    add ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = PROJECT_APPS + THIRD_APPS + DEFAULT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,6 +136,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 
 MEDIA_URL='/media/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
