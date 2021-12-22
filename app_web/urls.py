475c9b4621a821481dea9b00e5329db6a047065a
samueloliveiraf <samueldiablo73@gmail.com>
2021-12-22 11:44:43 -0300

    add

--- app_web/urls.py
+++ app_web/urls.py
@@ -30,14 +30,5 @@ urlpatterns = [
     path('', home, name='home')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-elif getattr(settings, 'FORCE_SERVE_STATIC', False):
-    settings.DEBUG = True
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    settings.DEBUG = False
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
