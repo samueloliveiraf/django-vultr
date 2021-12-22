068a71899da403dbb753ddb3b5cbed0d0c56611d
samueloliveiraf <samueldiablo73@gmail.com>
2021-12-22 01:08:21 -0300

    add

--- app_web/settings.py
+++ app_web/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = '/root/projetos-django/django-vultr/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 
