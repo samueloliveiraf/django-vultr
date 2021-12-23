2d6847db54165200fe9a11b2fc417b32de7f237d
samueloliveiraf <samueldiablo73@gmail.com>
2021-12-23 01:15:00 -0300

    add

--- app_web/settings.py
+++ app_web/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-+jg&ol7$z*ab9lp!98!c0#%g6&$g=$f-b&w9u&o#(#ynn+z6uv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
  
-ALLOWED_HOSTS = ['localhost', '155.138.230.147', 'djangoapp-estoque.online']
+ALLOWED_HOSTS = ['localhost', '155.138.230.147', '177.223.53.96', 'djangoapp-estoque.online']
 
 
 # Application definition
