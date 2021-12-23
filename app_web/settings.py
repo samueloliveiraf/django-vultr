e3b34fdd85ef915c83ed4e3853b24937cbcdb9de
samueloliveiraf <samueldiablo73@gmail.com>
2021-12-22 21:01:17 -0300

    add ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -135,12 +135,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 MEDIA_URL = '/media/'
 
