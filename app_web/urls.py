efc7f34a6cbd4b64f222f8f4667273d8194a0a18
samueloliveiraf <samueldiablo73@gmail.com>
2021-12-23 00:24:11 -0300

    add

--- app_web/urls.py
+++ app_web/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('', home, name='home')
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
