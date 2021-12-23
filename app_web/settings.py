528bdb1a94ca75d09b45ccfe3dbc4e64687681a9
samueloliveiraf <samueldiablo73@gmail.com>
2021-12-23 01:00:30 -0300

    add

--- app_web/settings.py
+++ app_web/settings.py
@@ -141,8 +141,6 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 MEDIA_URL='/media/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
