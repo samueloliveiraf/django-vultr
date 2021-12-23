e74cc29abd0602271742445a652445d02cdf01d1
samueloliveiraf <samueldiablo73@gmail.com>
2021-12-23 01:04:20 -0300

    add

--- app_web/settings.py
+++ app_web/settings.py
@@ -132,15 +132,18 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static'
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media')
+]
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 
-MEDIA_URL='/media/'
-
+MEDIA_URL = '/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
